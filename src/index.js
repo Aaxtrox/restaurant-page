4e2d96d966bb9b2fe9f08339d22835429d185e4f
Pawel Wojciechowski <pawelw@gmx.com>
2023-11-05 19:29:37 +0000

    Added Comments to Multiple Files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './normalize.css';
-import './styles.css';
+// Import CSS files for styling.
+import './normalize.css';  // Reset default styles.
+import './styles.css';     // Apply custom styles.
 
+// Import and execute the 'home' function to set up the homepage.
 import home from './pages/home';
-
 home();
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,24 @@
+// Import the 'background' image from the specified file path.
 import background from '../assets/background.png';
 
+// Define a function named 'home'.
 const home = function () {
-    //grab the content div
+    // Find the HTML element with the id 'content' and store it in the 'content' variable.
     const content = document.querySelector('#content');
-    //create div with id of home
+
+    // Create a new 'div' element and store it in the 'home' variable.
     const home = document.createElement('div');
+
+    // Set the 'id' attribute of the 'home' div to 'home'.
     home.id = 'home';
-    // put the background image in the div
+
+    // Set the 'backgroundImage' style property of the 'home' div
+    // to display the imported 'background' image as the background.
     home.style.backgroundImage = `url(${background})`;
-    // push the div to the content div
+
+    // Append the 'home' div as a child to the 'content' element.
     content.appendChild(home);
 }
 
+// Export the 'home' function as the default export of this module.
 export default home;

b162b7b22ffc9645843b78ea5db996a36c1b432c
Pawel Wojciechowski <pawelw@gmx.com>
2023-11-06 10:04:11 +0000

    Enhance home page content and styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 // Import the 'background' image from the specified file path.
-import background from '../assets/background.png';
+import background from '../assets/background1.png';
 
 // Define a function named 'home'.
 const home = function () {
@@ -18,6 +18,33 @@ const home = function () {
 
     // Append the 'home' div as a child to the 'content' element.
     content.appendChild(home);
+
+    // Create a new <div> element
+    const heroText = document.createElement('div');
+
+    // Assign the id attribute to the newly created element
+    heroText.id = 'hero-text';
+
+    // Append the 'heroText' element as a child to the 'home' element
+    home.appendChild(heroText);
+
+    // Create a new <h1> element, inside the 'heroText' element
+    const heroTitle = document.createElement('h1');
+
+    // Set the innerHTML of the 'heroTitle' element
+    heroTitle.innerHTML = 'Get Ready for Burger Heaven';
+
+    // Create a new <h2> element, inside the 'heroText' element
+    const heroSubtitle = document.createElement('h2');
+
+    // Set the innerHTML of the 'heroSubtitle' element
+    heroSubtitle.innerHTML = 'Where Taste Takes the Spotlight!';
+
+    // Append the 'heroTitle' element as a child to the 'heroText' element
+    heroText.appendChild(heroTitle);
+
+    // Append the 'heroSubtitle' element as a child to the 'heroText' element
+    heroText.appendChild(heroSubtitle);
 }
 
 // Export the 'home' function as the default export of this module.

8f4f940a1cc8bc6baa7ec9a41d619a27ac1cabb3
Pawel Wojciechowski <pawelw@gmx.com>
2023-11-14 12:30:24 +0000

    Add click event listeners to menu items and implement menu functionality

--- src/index.js
+++ src/index.js
@@ -10,3 +10,51 @@ import menu from './pages/menu';
 // Invoke the 'preloader' and 'home' functions.
 preloader();
 home();
+
+// Select all elements with class 'menu-item'
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Select all elements with class 'menu-item-ham'
+const menuItemsHam = document.querySelectorAll('.menu-item-ham');
+
+// Select the element with class 'btn-wrapper'
+const btnWrapper = document.querySelector('.btn-wrapper');
+
+// Add click event listeners to each element with class 'menu-item'
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        // Check the inner text of the clicked element and log a message accordingly
+        if (item.innerText === 'Home') {
+            console.log('Home')
+        } else if (item.innerText === 'Menu') {
+            console.log('Menu')
+        } else {
+            console.log('Contact');
+        }
+    });
+});
+
+// Add click event listeners to each element with class 'menu-item-ham'
+menuItemsHam.forEach((item) => {
+    item.addEventListener('click', () => {
+        // Check the inner text of the clicked element and log a message accordingly
+        if (item.innerText === 'Home') {
+            console.log('Home')
+        } else if (item.innerText === 'Menu') {
+            console.log('Menu')
+        } else {
+            console.log('Contact');
+        }
+    });
+});
+
+// Add a click event listener to the element with class 'btn-wrapper'
+btnWrapper.addEventListener('click', () => {
+    // Select the '#content' element and remove the '#home' element from it
+    const content = document.querySelector('#content');
+    const home = document.querySelector('#home');
+    content.removeChild(home);
+
+    // Call the 'menu' function
+    menu();
+});

c3976fa7577834c77a1905070f142d9217fb2204
Pawel Wojciechowski <pawelw@gmx.com>
2023-11-30 09:32:58 +0000

    Refactor carousel menu card

--- src/pages/carousel/carouselMenuCard.js
+++ src/pages/carousel/carouselMenuCard.js
@@ -1,17 +1,24 @@
+// Importing menu modules
 import menuBurgers from "../menu/burgers";
 import menuKids from "../menu/kids";
 import menuSides from "../menu/sides";
 import menuStarters from "../menu/starters";
 
+// Function for carousel menu card
 const carouselMenuCard = function () {
+    // Selecting the menu carousel element
     const menuCarousel = document.querySelector('.menu-carousel');
 
+    // Checking if the menu carousel exists
     if (menuCarousel) {
+        // Function to handle carousel loop
         loopCarousel();
 
+        // Selecting previous and next arrow elements
         const prevArrow = document.querySelector('.arrow.prev');
         const nextArrow = document.querySelector('.arrow.next');
 
+        // Adding click event listeners to arrows to loop the carousel
         prevArrow.addEventListener('click', () => {
             loopCarousel();
         });
@@ -22,43 +29,49 @@ const carouselMenuCard = function () {
     }
 }
 
+// Function to create and display burgers menu
 function burgers () {
     const menu = document.querySelector('#menu');
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-burgers');
     menu.appendChild(menuDiv);
 
-    menuBurgers();
+    menuBurgers(); // Display burgers menu content
 }
 
+// Function to create and display starters menu
 function starters () {
     const menu = document.querySelector('#menu');
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-starters');
     menu.appendChild(menuDiv);
 
-    menuStarters();
+    menuStarters(); // Display starters menu content
 }
 
+// Function to create and display kids menu
 function kids () {
     const menu = document.querySelector('#menu');
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-kids');
     menu.appendChild(menuDiv);
 
-    menuKids();
+    menuKids(); // Display kids menu content
 }
 
+// Function to create and display sides menu
 function sides () {
     const menu = document.querySelector('#menu');
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-sides');
     menu.appendChild(menuDiv);
 
-    menuSides();
+    menuSides(); // Display sides menu content
 }
 
+// Function to handle carousel loop based on selected item
 function loopCarousel () {
+    // Selecting the carousel menu items
     const menu = document.querySelector('#menu');
     const carouselMenuItems = document.querySelectorAll('.carousel-menu-item');
     const menuStarters = document.querySelector('.menu-starters');
@@ -66,59 +79,66 @@ function loopCarousel () {
     const menuSides = document.querySelector('.menu-sides');
     const menuBurgers = document.querySelector('.menu-burgers');
 
+    // Looping through the carousel menu items
     carouselMenuItems.forEach((item) => {
+        // Checking if the item has the 'active' class
         if (item.classList.contains('active')) {
             if (item.innerHTML === 'Starters') {
+                // Logic for displaying starters menu and handling duplicates
                 if (menuBurgers || menuKids || menuSides) {
-                    // grab the classlist name of the last child of #menu and remove it
+                    // Grab the classlist name of the last child of #menu and remove it
                     const lastChild = menu.lastChild;
                     menu.removeChild(lastChild);
 
-                    starters();
+                    starters(); // Display starters menu
                 } else if (menuStarters) {
-                    // do nothing
+                    // DO NOTHING
                 } else {
-                    starters();
+                    starters(); // Display starters menu
                 }
             } else if (item.innerHTML === 'Burgers') {
+                // Logic for displaying burgers menu and handling duplicates
                 if (menuStarters || menuKids || menuSides) {
-                    // grab the classlist name of the last child of #menu and remove it
+                    // Grab the classlist name of the last child of #menu and remove it
                     const lastChild = menu.lastChild;
                     menu.removeChild(lastChild);
 
-                    burgers();
+                    burgers(); // Display burgers menu
                 } else if (menuBurgers) {
-                    // do nothing
+                    // DO NOTHING
                 } else {
-                    burgers();
+                    burgers(); // Display burgers menu
                 }
             } else if (item.innerHTML === 'Kids Meals') {
+                // Logic for displaying kids menu and handling duplicates
                 if (menuBurgers || menuStarters || menuSides) {
-                    // grab the classlist name of the last child of #menu and remove it
+                    // Grab the classlist name of the last child of #menu and remove it
                     const lastChild = menu.lastChild;
                     menu.removeChild(lastChild);
                 
-                    kids();
+                    kids(); // Display kids menu
                 } else if (menuKids) {
-                    // do nothing
+                    // DO NOTHING
                 } else {
-                    kids();
+                    kids(); // Display kids menu
                 }
             } else if (item.innerHTML === 'Sides') {
+                // Logic for displaying sides menu and handling duplicates
                 if (menuBurgers || menuKids || menuStarters) {
-                    // grab the classlist name of the last child of #menu and remove it
+                    // Grab the classlist name of the last child of #menu and remove it
                     const lastChild = menu.lastChild;
                     menu.removeChild(lastChild);
                 
-                    sides();
+                    sides(); // Display sides menu
                 } else if (menuSides) {
-                    // do nothing
+                    // DO NOTHING
                 } else {
-                    sides();
+                    sides(); // Display sides menu
                 }
             }
         }
     });
 }
 
+// Exporting the carouselMenuCard function
 export default carouselMenuCard;

45669f70a31c34f6e51bb04cf4070b5a10ddbfcd
Pawel Wojciechowski <pawelw@gmx.com>
2023-11-30 11:59:14 +0000

    Add logo image and about text div to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,6 @@
+// Import the 'logo' image from the specified file path.
+import logo from '../assets/logo.png';
+
 // Function to create a about element
 const about = function () {
     // Get the 'content' element from the DOM
@@ -7,6 +10,17 @@ const about = function () {
     const about = document.createElement('div');
     about.id = 'about'; // Set the ID of the menu div
     content.appendChild(about); // Append the menu to the 'content' element
+
+    // Create a 'logo' img element
+    const logoImg = document.createElement('img');
+    logoImg.src = logo; // Set the source of the image
+    logoImg.alt = 'Logo'; // Set the alt text of the image
+    about.appendChild(logoImg); // Append the image to the 'about' element
+
+    // Create a 'aboutText' div element
+    const aboutText = document.createElement('div');
+    aboutText.id = 'aboutText'; // Set the ID of the 'aboutText' div
+    about.appendChild(aboutText); // Append the 'aboutText' div to the 'about' element
 }
 
 // Export the 'menu' function
